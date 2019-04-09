b2b2febd2282b23ccf048b5e4882e50cfad2754c
zinzan1 <zinzannzhao@gmail.com>
2019-04-09 20:32:04 +1200

    Removed unnecessary comments

--- src/test/java/softeng754/assignment3/DollarTest.java
+++ src/test/java/softeng754/assignment3/DollarTest.java
@@ -17,53 +17,38 @@ import org.junit.Test;
 public class DollarTest {
     @Test
     public void testDollarDivision() {
-// Given
         Dollar ten = new Dollar(10);
-// When
         Dollar result = ten.dividedBy(2);
-// Then
         assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
     }
     @Test(expected=ArithmeticException.class)
     public void testDollarDividedByZero() {
-// Given
         Dollar ten = new Dollar(10);
-// When
         ten.dividedBy(0);
     }
     @Test
     public void testFail() {
-// Given
 //        fail();
     }
 
     @Test
     public void testDollarMultiplication() {
-// Given
         Dollar ten = new Dollar(10);
-// When
         Dollar result = ten.times(2);
-// Then
         assertEquals(new Dollar(20), result);
     }
 
     @Test
     public void testDollarToString() {
-// Given
         Dollar ten = new Dollar(10);
-// When
         String result = ten.toString();
-// Then
         assertEquals("$10", result);
     }
 
     @Test
     public void testDollarEquals() {
-// Given
         Dollar ten = new Dollar(10);
         Dollar otherten = new Dollar(10);
-
-// Then
         assertEquals(ten, otherten);
     }
 }
